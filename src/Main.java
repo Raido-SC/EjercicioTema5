46d59052b6151dfea49b3fd00c32fed5423a3d9e
Raido-SC <ftblrichard@gmail.com>
2023-02-17 10:58:31 -0400

    subir proyecto

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        CocheCRUD cocheCrud = new CocheCRUDImpl();
+
+        System.out.println(cocheCrud);
+
+
+
+    }
+
+}
